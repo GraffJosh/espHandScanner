60ad563fa85043bea9007fe07a1abc4356aa2cf5
Josh Graff <github@joshuagraff.com>
2023-11-25 12:57:43 -0500

    bugfix

--- esphome/components/jpg_voice/JPGVoice.h
+++ esphome/components/jpg_voice/JPGVoice.h
@@ -12,20 +12,28 @@
 #include <SPI.h>
 #include <SD.h>
 
+//Variables to be used in the recording program, do not change for best
+#define SAMPLE_RATE 16000U
+#define SAMPLE_BITS 16
+#define WAV_HEADER_SIZE 44
+#define VOLUME_GAIN 2
+#define RECORD_TIME 10      // seconds, The maximum value is 240
+
+uint32_t record_size = (SAMPLE_RATE * SAMPLE_BITS / 8) * RECORD_TIME;
 namespace esphome{
     namespace handScanner{
         class JPGVoice : public Component {
             public:
             JPGVoice();
             void listDir(const char *dirname, uint8_t levels);
-            void uploadFile();
+            void uploadFile(const char *filename);
             void generate_wav_header(uint8_t *wav_header, uint32_t wav_size, uint32_t sample_rate);
             void recordAndUpload(void *arg);
             void startRecordAndUploadTask();
             private:
             
             File fileHandle;
-            const char defaultFilename[14] = "/recording.wav";
+            const char defaultFilename[15] = "/recording.wav";
         };
     }
 }
